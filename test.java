efd1194d3842f0f43f1eb0d68549d3bb81a83198
manas9874 <palmanas0408@gmail.com>
2025-06-16 17:10:56 +0530

    Add files via upload

---
+++ test.java
@@ -0,0 +1,11 @@
+//array intersection
+import java.util.Scanner;
+public class test {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n,i,j;
+        System.out.println("enter the range:");
+        
+        int a[]=new int[n]
+    }
+}
