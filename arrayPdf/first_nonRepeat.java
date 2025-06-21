583229306af59eac0f5b172b76455884b8de2cf3
manas9874 <palmanas0408@gmail.com>
2025-06-21 08:32:41 +0530

    Create first_nonRepeat.java

---
+++ arrayPdf/first_nonRepeat.java
@@ -0,0 +1,6 @@
+/*Description: Write a program to find the first non-repeating element in an array. 
+Sample Input: 
+[9, 4, 9, 6, 7, 4] 
+Sample Output: 
+6 
+Explanation: 6 is the first element that doesn't appear more than once in the array.*/
