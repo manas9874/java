5f2ad19d7dc92d30ee9b774bf6fc11a2cd7e6377
manas9874 <palmanas0408@gmail.com>
2025-06-21 12:28:10 +0530

    Update first_nonRepeat.java

--- arrayPdf/first_nonRepeat.java
+++ arrayPdf/first_nonRepeat.java
@@ -4,3 +4,28 @@ Sample Input:
 Sample Output: 
 6 
 Explanation: 6 is the first element that doesn't appear more than once in the array.*/
+
+import java.util.Scanner;
+
+public class first_nonRepeat {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n,i,j;
+        System.out.println("enter the range:");
+        n=sc.nextInt();
+        int a[]=new int[n];
+        System.out.println("enter numbers");
+        for(i=0;i<n;i++){
+            a[i]=sc.nextInt();
+        }
+        sc.close();
+        for(i=0;i<a.length-1;i++){
+            for(j=i+1;i<a.length;j++){
+                if(a[i]==a[j]){
+                    System.out.println(a[i]);
+                    break;
+                }
+            }
+        }
+    }
+}
